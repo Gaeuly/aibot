76584b19fa3cf10480680d5e158336842772f20d
Gaeul <143046394+Gaeuly@users.noreply.github.com>
2025-08-26 13:44:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Ganti dengan token akun Discord alternatifmu
-    token: "NTAwMjkzMzY1NDk0MDU0OTMy.G870MM.c4OTulizWX2PBQ9WYEnnrKPkD0FjE63lwSBhzY",
+    token: "MTM0MzE2ODk2ODIzNTU1MjgxNA.GLFDlN.-mngSZCX9719PNDGZO5iVFRx6tpmW8FQpjfrss",
 
     // (Opsional) Ganti nama ini sesuai kepribadian AI yang kamu mau
     aiName: "Asisten AI",
